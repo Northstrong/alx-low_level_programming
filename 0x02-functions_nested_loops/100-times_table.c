caf1da693a386392e2df872efbbe034af63dec5f
Northstrong <chizzy48@gmail.com>
2023-02-22 12:29:27 -0800

    100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * To computes and prints the sum of all multiples of 3 or 5 below 1024
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+
+	i = 0;
+	n = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+	{
+		n += i;
+	}
+		i++;
+	}
+	printf("%d\n", n);
+	return (0);
+}
