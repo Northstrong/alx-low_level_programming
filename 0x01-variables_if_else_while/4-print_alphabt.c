6b61a3b1026b9cfad6791b91f1f510ba3b5e9080
Northstrong <chizzy48@gmail.com>
2023-02-24 15:15:36 -0800

    alph

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A program that prints the alphabet in lowercase
+ * Return: 0 always (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph == 'q' || alph == 'e')
+			continue;
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
