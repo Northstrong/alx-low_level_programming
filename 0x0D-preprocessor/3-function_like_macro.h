1917cdac3ec343ab4f575e68b776c5c9675e5795
Northstrong <chizzy48@gmail.com>
2023-03-21 15:16:23 -0700

    preprocessor

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#include <stdio.h>
+
+/**
+ * File name: 3-function_like_macro.h
+ * Desc: To computes the absolute value of a number x.
+ * @x: The number
+ */
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H*/
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,16 @@
+#ifndef SUM_H
+#define SUM_H
+
+#include <stdio.h>
+
+/**
+ * File name: 4-sum.h
+ * Desc: A function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y
+ * @x: First number
+ * @y: Second number
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H*/
